063d6084a8d9d2dc683358b6310e1cc54ae3263d
Pavan Kumar Patruni <>
2019-09-04 03:15:42 +0530

    Updates

---
+++ precache-manifest.44241d64c1d2bdf832cb28248b25d159.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146ec9340e864ebea44ab858b041053e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ea9a1776e1e6b41723a",
+    "url": "/static/css/main.cc9c58ea.chunk.css"
+  },
+  {
+    "revision": "c29329048b6fc9d98005",
+    "url": "/static/js/2.44525d9b.chunk.js"
+  },
+  {
+    "revision": "1ea9a1776e1e6b41723a",
+    "url": "/static/js/main.0536b5bd.chunk.js"
+  },
+  {
+    "revision": "6b3698aa9e46f3f405a1",
+    "url": "/static/js/runtime~main.57e4fa1b.js"
+  }
+]);
